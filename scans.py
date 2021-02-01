3e7de98c3fb5f66c9c451ed4cec2e1481afd718b
Michael Farber Brodsky <taco@mailo.com>
2021-02-01 17:56:33 +0200

    Add work in progress scans listing

---
+++ scans.py
@@ -0,0 +1,10 @@
+import api
+from bs4 import BeautifulSoup
+from util import parse_table
+
+def scans_list_cli(args, auth_data):
+    html = api.login_and_fetch_or_exit("https://www.ims.tau.ac.il/Tal/Scans/Scans_P.aspx?first=yes", auth_data)
+    soup = BeautifulSoup(html, features="lxml")
+    table = soup.find("table")
+    tochniot = parse_table(table)
+    print(tochniot)
---
+++ util.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+def list_or_first_value(l):
+    if len(l) == 1:
+        return l[0]
+    else:
+        return l
+
+def parse_table(table):
+    rows = table.find_all("tr")
+    if len(rows) == 0:
+        return {}
+    else:
+        first_row = rows[0]
+        columns = [x.text for x in first_row.select("td,th")]
+        result = []
+        for row in rows[1:]:
+            values = [list_or_first_value(list(x.children)) or x for x in row.select("td,th")]
+            result.append({columns[i]: values[i] for i in range(len(columns))})
+        return result
+
